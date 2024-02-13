ed045e77a5cc356758499d372548f8843cf90acb
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-02-13 19:25:42 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,20 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "Sure ka na ba?",
+      "What if I mag ask ulit ako?",
+      "Please na kasi",
+      "Libre kita ube cake",
+      "What if libre kita ng chocolate ice cream?",
+      "PLEASE ALIPOT",
+      "Sigeee na 🥺",
+      "Pag di ka pumayag mag cry ako! 😭",
+      "Sureee ka na ba talaga?",
+      "Please na talaga",
+      "Please na talaga alipot",
+      "Gusto ko ibigay buhay na gusto mo 🎶",
+      "Chibog tayo? 🍔🍟🍕",
+      "Lutuan kita carbonara? 🍝",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -38,7 +37,9 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">
+            I love you alipotmylovesosweet! 😍🥰💕
+          </div>
         </>
       ) : (
         <>
